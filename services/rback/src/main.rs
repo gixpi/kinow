db618d17e7ef2f25a90dce8292cf4ae4649f653e
gixpi <gixpiop@gmail.com>
2023-08-26 22:55:15 +0330

    rback folders

--- services/rback/src/main.rs
+++ services/rback/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+
+use clap::Parser;
+use rback::app::config::ParseConfig;
+use rback::app::database;
+use tonic::transport::Server;
+
+#[tokio::main]
+async fn main() {
+    
+    println!("[INFO] Parse Input Config");
+    let parsed =  ParseConfig::parse();
+    println!("[INFO] Connecting To PostgresDB...");
+    let pg_db_pool = database::postgres_connection(parsed.db_username, parsed.db_password, parsed.db_host, parsed.db_port,parsed.db_name)
+    .await.unwrap();
+    println!("[INFO] Connected To PostgresDB!");
+    
+    let pg_db_pool = Arc::new(pg_db_pool);
+    
+    println!("[INFO] Init Services");
+    // init services
+
+    // authentication service
+
+    println!("[INFO] Running Server On {}",parsed.listen_address);
+    // Server::builder()
+    // .serve(parsed.listen_address.parse().expect("could not parse the listener address"))
+    // .await
+    // .unwrap()
 }
